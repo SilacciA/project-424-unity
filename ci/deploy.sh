8caea6744918d328f3af29e53f73621e4e6c683d
Alessandro Silacci <silacci.alessandro@gmail.com>
2021-03-07 00:57:46 +0100

    Added debug to deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-apk add --update --no-cache openssh sshpass && sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
+set -e
+
+apk add --update --no-cache openssh sshpass
+
+sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
