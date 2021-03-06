e7aa78b71812f439b07e9c39b9c0021ad0d8dcf1
Alessandro Silacci <silacci.alessandro@gmail.com>
2021-03-07 00:21:08 +0100

    Created deloy script

---
+++ ci/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apk add --update --no-cache openssh sshpass && sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
--- ci/docker_deploy.sh
+++ ci/docker_deploy.sh
@@ -12,4 +12,4 @@ docker run \
   -w /build \
   -v $(pwd):/build/ \
   $IMAGE_NAME \
-  apk add --update --no-cache openssh sshpass && sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
+  ./ci/deploy.sh
