f1378d265f2122ac5cfa037c558a1dc03e1641e3
Alessandro Silacci <silacci.alessandro@gmail.com>
2021-03-08 01:03:33 +0100

    Removed deployment stage since it is not possible to pass data through stages

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-apk add --update --no-cache openssh sshpass
+#apk add --update --no-cache openssh sshpass
 
-ls -al .
+#ls -al .
 
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
