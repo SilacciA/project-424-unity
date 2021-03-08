096b738c2f6153cabdf78a45b37e4ecfbf33bdcf
Alessandro Silacci <alessandro.silacci@edu.hefr.ch>
2021-03-09 00:18:18 +0100

    Added debug line to deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 #apk add --update --no-cache openssh sshpass
 
-#ls -al .
+ls -al .
 
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
+
+echo "Deployed data"
