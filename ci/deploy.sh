080df5b6ac18c848a67ccdaf0ce7b87a6dbed4bb
Alessandro Silacci <alessandro.silacci@edu.hefr.ch>
2021-04-02 17:23:38 +0200

    Corrected sed command

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,8 +5,9 @@ set -e
 #apk add --update --no-cache openssh sshpass
 
 ls -al Builds/$BUILD_TARGET/$BUILD_NAME/Build/
-#sudo mv "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb" "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb.backup"
-sudo sed -e '/abort("To use dlopen/ s/^#*/\/\//' "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb.backup" > "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb"
+sudo chown -R travis:travis Builds/$BUILD_TARGET/$BUILD_NAME/Build/
+mv "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb" "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb.backup"
+sed -e '/abort("To use dlopen/ s/^#*/\/\//' "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb.backup" > "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb"
 
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH/Backup
