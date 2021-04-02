c7186c2c8556683c48972d9271baef941c49add8
Alessandro Silacci <alessandro.silacci@edu.hefr.ch>
2021-04-02 12:38:28 +0200

    Corrected sed command

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,8 +4,8 @@ set -e
 
 #apk add --update --no-cache openssh sshpass
 
-ls -al Builds/$BUILD_TARGET/$BUILD_NAME/
-sed -e '/abort("To use dlopen/ s/^#*/\/\//' -i Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb
+ls -al Builds/$BUILD_TARGET/$BUILD_NAME/Build/
+sed -e '/abort("To use dlopen/ s/^#*/\/\//' -i "Builds/$BUILD_TARGET/$BUILD_NAME/Build/$BUILD_NAME.wasm.framework.unityweb"
 
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME/Build $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH
 sshpass -p $DEPLOYMENT_TARGET_PASSWORD scp -o "StrictHostKeyChecking no" -r Builds/$BUILD_TARGET/$BUILD_NAME $DEPLOYMENT_TARGET_USER@$DEPLOYMENT_TARGET_ADDRESS:$DEPLOYMENT_TARGET_PATH/Backup
